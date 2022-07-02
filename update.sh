7e8033df840718ec411e114d20ed3b7b5a47be96
Todd Austin <toddaustin07@yahoo.com>
2022-07-02 15:03:32 -0500

    Delete update.sh

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#scp -P 1117 prometheus18x@192.168.1.145:/home/prometheus18x/smartthings/smartapps.js smartapps.js
-scp -P 1117 prometheus18x@192.168.1.145:/home/prometheus18x/smartthings/mqttout.js mqttout.js
-scp -P 1117 prometheus18x@192.168.1.145:/home/prometheus18x/smartthings/common.js common.js
-scp -P 1117 prometheus18x@192.168.1.145:/home/prometheus18x/smartthings/locales/mqttout_en.json mqttout_en.json
-
